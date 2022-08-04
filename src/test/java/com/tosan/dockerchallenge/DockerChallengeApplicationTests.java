4ac58d36b4af253b361f1e2a2e2cd26f5d943197
MohammadAsgharian <marzieh.vahid@gmail.com>
2022-08-04 15:57:31 +0430

    add unit test

--- src/test/java/com/tosan/dockerchallenge/DockerChallengeApplicationTests.java
+++ src/test/java/com/tosan/dockerchallenge/DockerChallengeApplicationTests.java
@@ -1,13 +1,39 @@
 package com.tosan.dockerchallenge;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class DockerChallengeApplicationTests {
+public class DockerChallengeApplicationTests extends AbstractTest {
+
 
     @Test
-    void contextLoads() {
+    public void helloworldTest() throws Exception {
+        setUp();
+        String uri = "/helloworld";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Hello Stranger");
+    }
+
+    @Test
+    public void authorTest() throws Exception {
+        setUp();
+        String uri = "/author";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Marzieh Vahid");
     }
 
 }
