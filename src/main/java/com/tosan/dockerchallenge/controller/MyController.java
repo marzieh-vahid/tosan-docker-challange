6d5d477a47f1a9671cc0d30a3b88296fb3cd6ad6
MohammadAsgharian <marzieh.vahid@gmail.com>
2022-08-04 16:06:13 +0430

    update log

--- src/main/java/com/tosan/dockerchallenge/controller/MyController.java
+++ src/main/java/com/tosan/dockerchallenge/controller/MyController.java
@@ -6,27 +6,29 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @Slf4j
 public class MyController {
 
     @GetMapping(path = "/helloworld", produces = "application/json")
     public String HelloWorld() {
-        log.info("helloworld service is up....");
+        log.info(new Date() + ".....helloworld service is up....");
         return "Hello Stranger";
     }
 
     @ResponseBody
     @GetMapping(path = "/say/helloworld", produces = "application/json")
     public String sayHello(@RequestParam String name) {
-        log.info("now say hello service is up.....");
-        log.info("the entered name is:... " + name);
+        log.info(new Date() + ".....now say hello service is up.....");
+        log.info(new Date() + ".....the entered name in Request is:... " + name);
         return "Hello " + name;
     }
 
     @GetMapping(path = "/author", produces = "application/json")
     public String myNameIs() {
-        log.info("my name is.....");
+        log.info(new Date() + ".....my name is.....");
         return "Marzieh Vahid";
     }
 
